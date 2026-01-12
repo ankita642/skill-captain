fc57722d47dc2681e4beb5fa33a62dfaddf1dd41
ankita642 <jadhavankita116@gmail.com>
2026-01-12 22:28:29 +0530

    login form validation

---
+++ CSS STYLE/day1.js
@@ -0,0 +1,76 @@
+console.log("Hello, World!");
+
+//string datatype
+let greeting = "hello world!";
+console.log(greeting);
+
+//number datatype
+let year = 2024;
+console.log(year);
+
+//boolean datatype
+let isLearning = true;
+console.log(isLearning);
+
+//undefined datatype
+let undefinedVariable;
+console.log(undefinedVariable);
+
+//null datatype
+let nullVariable = null;
+console.log(nullVariable);
+
+//object datatype
+let person = {
+    name: "John",
+    age: 30
+};
+console.log(person);
+
+//array datatype
+let colors = ["red", "green", "blue"];
+console.log(colors);
+
+//loops
+for (let i = 0; i < 5; i++) {
+    console.log("Iteration number: " + i);
+}
+
+while (year < 2025) {
+    console.log("The year is: " + year);
+    year++;
+}
+
+do {
+    console.log("This will run at least once.");
+}
+while (false);
+
+//conditional statements
+if (isLearning) {
+    console.log("Keep up the good work!");
+} else {
+    console.log("Time to start learning!");
+}
+
+//function
+function add(a, b) {
+    return a + b;
+}
+let sum = add(5, 10);
+console.log("The sum is: " + sum);
+
+//arrow function
+const multiply = (x, y) => x * y;
+let product = multiply(4, 5);
+console.log("The product is: " + prod
+
+ 
+
+
+
+
+
+
+
+
